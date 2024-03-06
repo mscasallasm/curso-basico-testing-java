ce7d9ff845bca0883ddf6bdccc61b23ef4fe5f4e
mscasallasm <stiven_casallas@hotmail.com>
2024-03-06 11:20:54 -0500

    Clase 5: Test unitario con JUnit

--- src/test/java/org/platzi/javatest/util/StringUtilTest.java
+++ src/test/java/org/platzi/javatest/util/StringUtilTest.java
@@ -1,19 +1,14 @@
 package org.platzi.javatest.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
 
-//        String result = StringUtil.repeat("hola",3);
-//        assertEquals(result, "holaholahola");
-        assertEquals(StringUtil.repeat("hola",3), "holaholahola");
-
-//        String result2 = StringUtil.repeat("hola",1);
-        assertEquals(StringUtil.repeat("hola",1),"hola");
+    @Test
+    public void testRepeat() {
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola",3));
+        Assert.assertEquals("hola", StringUtil.repeat("hola",1));
     }
 
-    private static void assertEquals(String actual, String expected) {
-        if (!actual.equals(expected)) {
-            throw new RuntimeException(actual + " is not equal to expected " + expected);
-        }
-    }
 }

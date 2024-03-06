21637972d7aa7509e76ba9fd3c43d1058f7f0729
mscasallasm <stiven_casallas@hotmail.com>
2024-03-06 11:00:18 -0500

    Clase 3: Tests unitarios

---
+++ src/main/java/org/platzi/javatest/util/StringUtil.java
@@ -0,0 +1,15 @@
+package org.platzi.javatest.util;
+
+public class StringUtil {
+
+    public static String repeat(String str, int times) {
+
+        String result = "";
+
+        for (int i = 0; i < times; i++) {
+            result += str;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/org/platzi/javatest/util/StringUtilTest.java
@@ -0,0 +1,17 @@
+package org.platzi.javatest.util;
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+
+        String result = StringUtil.repeat("hola",3);
+        if (!result.equals("holaholahola")) {
+            System.out.println("ERROR");
+        }
+
+
+        String result2 = StringUtil.repeat("hola",1);
+        if (!result2.equals("hola")) {
+            System.out.println("ERROR");
+        }
+    }
+}

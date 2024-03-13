25533c1e9b8295d45e25ddff9a5894d4b3d72da4
mscasallasm <stiven_casallas@hotmail.com>
2024-03-13 11:26:35 -0500

    Clase 13: Ejemplo de TDD: Calcular el año bisiesto.

---
+++ src/main/java/org/platzi/javatest/util/DateUtil.java
@@ -0,0 +1,10 @@
+package org.platzi.javatest.util;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year) {
+
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+}

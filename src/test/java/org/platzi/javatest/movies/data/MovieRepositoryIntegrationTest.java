db5bf15f86264007006ac7feda55c4a263c7867b
mscasallasm <stiven_casallas@hotmail.com>
2024-03-20 11:39:33 -0500

    Creación de la base de datos y tests de integración con bases de datos

---
+++ src/test/java/org/platzi/javatest/movies/data/MovieRepositoryIntegrationTest.java
@@ -0,0 +1,40 @@
+package org.platzi.javatest.movies.data;
+
+import org.junit.Test;
+import org.platzi.javatest.movies.model.Genre;
+import org.platzi.javatest.movies.model.Movie;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+import javax.sql.DataSource;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+public class MovieRepositoryIntegrationTest {
+
+    @Test
+    public void load_all_movies() throws SQLException {
+
+        DataSource dataSource = new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
+
+        ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        MovieRepositoryJdbc movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+
+        Collection<Movie> movies = movieRepository.findAll();
+
+        assertEquals(Arrays.asList(
+                new Movie(1,"Dark Knight", 152, Genre.ACTION),
+                new Movie(2,"Memento", 113, Genre.THRILLER),
+                new Movie(3,"Matrix", 136, Genre.ACTION)
+        ), movies);
+    }
+}
